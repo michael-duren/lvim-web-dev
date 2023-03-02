8142ee410c95cf505b9d954e798086d74defb7de
Michael Duren <michaeld@michaelduren.com>
2023-03-02 10:34:39 -0600

    cleaned up config.lua

--- lua/michael/plugin-settings/lsp.lua
+++ lua/michael/plugin-settings/lsp.lua
@@ -10,3 +10,36 @@ lspconfig["emmet_ls"].setup({
 lspconfig["csharp_ls"].setup({
 	filetypes = { "cs" },
 })
+
+--formatting and linting
+lvim.log.level = "info"
+lvim.format_on_save = {
+	enabled = true,
+	timeout = 5000,
+}
+
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "stylua" },
+	{
+		command = "prettier",
+		extra_args = { "--print-width", "100" },
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	},
+	{
+		command = "csharpier",
+		filetypes = { "cs" },
+	},
+})
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ command = "flake8", filetypes = { "python" } },
+	{
+		command = "shellcheck",
+		args = { "--severity", "warning" },
+	},
+	{
+		command = "eslint",
+		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	},
+})
